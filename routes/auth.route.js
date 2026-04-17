9d0d521a6029b08f89638dc63c9b4b07c02943fa
autotest110-cmyk <autotest110@gmail.com>
2026-04-17 18:25:16 +0530

    Backend changes otp

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -89,12 +89,18 @@ router.post("/register", async (req, res) => {
         subject: "Verify Email",
         html: `<h2>Your OTP: ${otp}</h2>`,
       });
+       return res.json({
+        success: true,
+        message: "OTP sent successfully",
+        email,
+      });
     } catch (error) {
       console.error("❌ EMAIL ERROR:", error);
 
-      return res.status(500).json({
+     
+         return res.status(400).json({
         success: false,
-        message: "Failed to send OTP. Please check email.",
+        message: "Invalid email or unable to send OTP",
       });
     }
 
